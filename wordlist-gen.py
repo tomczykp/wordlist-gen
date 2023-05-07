bef2482f404f38ebe373cec6704dd8752338a2d8
Piotr <49697739+tomczykp@users.noreply.github.com>
2023-05-07 23:43:15 +0200

    added words separator

--- wordlist-gen.py
+++ wordlist-gen.py
@@ -2,11 +2,12 @@
 from typing import List
 from itertools import permutations
 
-output_filename = "wordlist.lst"
-base_wordlist = ["foo", "bar", "baz"]
-word_len_limit = 8
-numbering_limit = 2023
-substites = {
+output_filename = "proto-wordlist.lst"
+base_wordlist = ["name", "surname", "security", "itsec", "password", "haslo"]
+delims = ["", ".", "-", "_", "+", "=", " ", "0"]
+LIMIT = 15
+numbering_limit = 10000
+substitutes = {
         'a': ['4', '@'],
         'e': ['3'],
         'i': ['1'],
@@ -16,14 +17,21 @@ substites = {
         'O': ['0'],
     }
 
-def base() -> List[str]:
+def check(t: str):
+    return LIMIT == len(t) 
+    
+def checkClose(t: str):
+    return LIMIT - 2 <= len(t) <= LIMIT + 2
+
+def base(delim) -> List[str]:
     result = []
     for group in permutations(base_wordlist):
         for i in range(len(group)):
-            t = ''.join(x for x in group[i:])
-            if len(t) <= word_len_limit and len(t) > 0:
+            t = f"{delim}".join(x for x in group[i:])
+            if checkClose(t):
                 result.append(t)
     return result
+
 
 def leeting(wordlist: List[str]) -> List[str]:
     def leetspeak(instr: str):
@@ -32,27 +40,32 @@ def leeting(wordlist: List[str]) -> List[str]:
         else:
             first = instr[:1]
             for sub_replace in leetspeak(instr[1:]):
-                if first in substites:
-                    for changed in substites[first]:
+                if first in substitutes:
+                    for changed in substitutes[first]:
                         yield changed + sub_replace
                 yield first + sub_replace
-    result: List[str] = []    
-    for word in wordlist:
-        result.extend([x for x in leetspeak(word)])
-    return result 
-    
-def numbering(wordlist: List[str]) -> List[str]:
     result: List[str] = []
     for word in wordlist:
+        result.extend([x for x in leetspeak(word) if checkClose(x)])
+    return result 
+
+
+def numbering(wordlist: List[str]) -> List[str]:
+    result: List[List[str]] = []
+    numbering_limit_chars: int = len(str(numbering_limit))
+    for word in wordlist:
         t: List[str] = []
+        if check(word):
+            t.append(word)
         for number in range(numbering_limit):
-            t.append(f"{word}{number:02d}")
-            t.append(f"{word}{number:03d}")
-            t.append(f"{word}{number:04d}")
-            t.append(f"{word}{number}")
-        result.extend(t)
-    return result
-    
+            for i in range(numbering_limit_chars - len(str(number)) + 1):
+                w: str = f"{word}{'0'*i}{number}"
+                if check(w):
+                    t.append(w)
+        result.append(t)
+    return [i for sub in result for i in sub]
+
+
 def spongebobing(wordlist: List[str]) -> List[str]:
     def all_casings(instr: str):
         if not instr:
@@ -60,29 +73,35 @@ def spongebobing(wordlist: List[str]) -> List[str]:
         else:
             first = instr[:1]
             for sub_casing in all_casings(instr[1:]):
-                yield first.lower() + sub_casing
-                yield first.upper() + sub_casing
+                if first.isalpha():
+                    yield first.lower() + sub_casing
+                    yield first.upper() + sub_casing
+                else:
+                    yield first + sub_casing
     result: List[str] = []
     for word in wordlist:
-        result.extend([x for x in all_casings(word)])
-    return result 
+        result.extend([x for x in all_casings(word) if checkClose(x)])
+    return result
         
     
 def run():
-    print("Generating wordlist.lst")
-    wordlist: List[str] = base()
-    print(f"Generated {len(wordlist)} permutations of keywords")
-    wordlist = spongebobing(wordlist)
-    print(f"Generated {len(wordlist)} mixed cases")
-    wordlist = leeting(wordlist)
-    print(f"Generated {len(wordlist)} words with letters substitutions")
-    wordlist = numbering(wordlist)
-    print(f"Generated {len(wordlist)} words with added numbers")
-    outBuffer = "".join(f"{word}\n" for word in wordlist)
-    print(f"Result length: {len(wordlist)}")
-    with open(output_filename, 'w') as file:
+    for delim in delims:
+        print(f"Generating {output_filename} for `{delim}`")
+        wordlist: List[str] = base(delim)
+        print(f"Generated {len(wordlist)} permutations of keywords")
+        wordlist = spongebobing(wordlist)
+        print(f"Generated {len(wordlist)} mixed cases")
+        wordlist = leeting(wordlist)
+        print(f"Generated {len(wordlist)} words with letters substitutions")
+        wordlist = numbering(wordlist)
+        print(f"Generated {len(wordlist)} words with added numbers")
+        writeFile(wordlist)
+
+def writeFile(wordlist: List[str]) -> None:
+    outBuffer: str = "".join(f"{word}\n" for word in wordlist)
+    with open(output_filename, 'a') as file:
         file.write(outBuffer)
+        
 
 if __name__ == "__main__":
     run()
-    
